e2b510d3e7f76e63ac7961fce77f1842fe9fff94
Tomas Liñeira Abal <tlineira@gmail.com>
2025-06-22 17:16:00 -0300

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,12 +3,12 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 exports.register = (req, res) => {
-  const { nombre, apellido, email, telefono, dni, password, ubicacion , fechanacimiento} = req.body;
+  const { nombre, apellido, email, telefono, dni, password} = req.body;
   const hash = bcrypt.hashSync(password, 10);
 
   db.run(
-    `INSERT INTO users (nombre, apellido, email, telefono, dni, password, ubicacion, fechanacimiento) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
-    [nombre, apellido, email, telefono, dni, hash, ubicacion, fechanacimiento],
+    `INSERT INTO users (nombre, apellido, email, telefono, dni, password) VALUES (?, ?, ?, ?, ?, ?)`,
+    [nombre, apellido, email, telefono, dni, hash],
     function (err) {
       if (err) return res.status(400).json({ error: err.message });
       res.status(201).json({ id: this.lastID, nombre, email });
